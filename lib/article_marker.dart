1e464016ebcfda1429459502c26962e4b7df75e5
matthew-matvei <matthew.d.james87@gmail.com>
2020-11-04 13:25:35 +1000

    Handle directing user to article from preview

--- lib/article_marker.dart
+++ lib/article_marker.dart
@@ -1,68 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'package:either_option/either_option.dart';
 
+import 'article_preview.dart';
 import 'article_result.dart';
-import 'fetcher.dart' as fetcher;
 
-void _handlePressed(ArticleResult article) async {
-  final endpoint = Uri.https(
-      'en.wikipedia.org',
-      'w/api.php',
-      {
-        'action': 'query',
-        'prop': 'info',
-        'inprop': 'url',
-        'titles': article.title,
-        'format': 'json',
-      }
-  );
-
-  (await fetcher.get(endpoint))
-      .fold(
-          (error) {
-        print("${error.errorCode}: ${error.errorMessage}");
-        return None<String>();
-      },
-          (result) =>
-          Some(_FetchPageInfoResult
-              .fromJson(result['query']['pages'][article.pageId.toString()])
-              .uri))
-      .fold(
-          () => print("Couldn't acquire url"),
-          (urlString) async =>
-      {
-        if (await canLaunch(urlString)) {
-          await launch(urlString)
-        } else {
-            print("Couldn't launch url")
-          }
-      });
+void _handlePressed(ArticleResult article, BuildContext context) async {
+  showModalBottomSheet<void>(
+      context: context,
+      builder: (_) => ArticlePreview(
+          article: article,
+          context: context
+      ),
+      isScrollControlled: true);
 }
 
 class ArticleMarker extends Marker {
 
   ArticleMarker(ArticleResult article) : super(
       point: article.coordinates,
-      builder: (_) =>
+      builder: (context) =>
           Container(
               child: IconButton(
                 icon: Icon(Icons.pin_drop, size: 35),
                 color: Colors.redAccent,
-                onPressed: () => _handlePressed(article),
+                onPressed: () => _handlePressed(article, context),
                 iconSize: 35,
               )
           )
   );
-}
-
-class _FetchPageInfoResult {
-
-  final String uri;
-
-  const _FetchPageInfoResult({this.uri});
-
-  factory _FetchPageInfoResult.fromJson(Map<String, dynamic> json) =>
-      _FetchPageInfoResult(uri: json['canonicalurl']);
 }
--- lib/article_preview.dart
+++ lib/article_preview.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:either_option/either_option.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'article_result.dart';
 import 'fetcher.dart' as fetcher;
@@ -10,8 +11,9 @@ import 'fetcher.dart' as fetcher;
 class ArticlePreview extends StatefulWidget {
 
   final ArticleResult article;
+  final BuildContext context;
 
-  ArticlePreview({Key key, this.article}) : super(key: key);
+  ArticlePreview({Key key, this.article, this.context}) : super(key: key);
 
   @override
   _ArticlePreviewState createState() => _ArticlePreviewState();
@@ -61,6 +63,41 @@ Future<String> _fetchIntroFor(ArticleResult article) async {
           (result) => result['query']['pages'][article.pageId.toString()]['extract']);
 }
 
+void _readArticle(ArticleResult article) async {
+  final endpoint = Uri.https(
+      'en.wikipedia.org',
+      'w/api.php',
+      {
+        'action': 'query',
+        'prop': 'info',
+        'inprop': 'url',
+        'titles': article.title,
+        'format': 'json',
+      }
+  );
+
+  (await fetcher.get(endpoint))
+      .fold(
+          (error) {
+        print("${error.errorCode}: ${error.errorMessage}");
+        return None<String>();
+      },
+          (result) =>
+          Some(_FetchPageInfoResult
+              .fromJson(result['query']['pages'][article.pageId.toString()])
+              .uri))
+      .fold(
+          () => print("Couldn't acquire url"),
+          (urlString) async =>
+      {
+        if (await canLaunch(urlString)) {
+          await launch(urlString)
+        } else {
+            print("Couldn't launch url")
+          }
+      });
+}
+
 class _ArticlePreviewState extends State<ArticlePreview> {
 
   bool _fetchingIntro = true;
@@ -91,49 +128,90 @@ class _ArticlePreviewState extends State<ArticlePreview> {
   Widget build(BuildContext context) {
     return DraggableScrollableSheet(
       builder: (context, scrollController) =>
-          Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                    Row(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            child: _thumbnailUrl.fold(
-                                    () => Icon(Icons.image, size: 50),
-                                    (url) => CachedNetworkImage(
-                                        imageUrl: url,
-                                        placeholder: (_, __) => Icon(Icons.image, size: 50),
-                                        errorWidget: (_, __, ___) => Icon(Icons.error),
-                                        width: 50))
-                        ),
-                        Expanded(
-                            child: Text(
-                              widget.article.title,
-                              style: TextStyle(fontSize: 20)
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: _thumbnailUrl.fold(
+                                  () => Icon(Icons.image, size: 50),
+                                  (url) => CachedNetworkImage(
+                                  imageUrl: url,
+                                  placeholder: (_, __) => Icon(Icons.image, size: 50),
+                                  errorWidget: (_, __, ___) => Icon(Icons.error),
+                                  width: 50))
+                          ),
+                          Expanded(
+                              child: Text(
+                                  widget.article.title,
+                                  style: TextStyle(fontSize: 20)
+                              )
                           )
-                        )
-                      ],
-                    ),
-                  Divider(),
-                  Expanded(
-                      child: ListView(
-                          controller: scrollController,
-                          children: [
-                              _fetchingIntro
-                                  ? Center(
+                        ],
+                      ),
+                      Divider(),
+                      Expanded(
+                          child: ListView(
+                              controller: scrollController,
+                              children: [
+                                _fetchingIntro
+                                    ? Center(
                                       child: CircularProgressIndicator(),
                                       heightFactor: 3)
-                                  : Html(data: _intro)
-                          ])
+                                    : Html(data: _intro),
+                                Container(height: 40)
+                              ]),
+                      )
+                    ]
+                ),
+              ),
+              Positioned(
+                  child: Container(
+                      width: double.infinity,
+                      height: 40,
+                      color: Color.fromARGB(200, 255, 255, 255),
+                      child: Center(
+                          child: FlatButton(
+                              child: Text("READ MORE"),
+                              textColor: Colors.blueAccent,
+                              height: double.infinity,
+                              onPressed: () {
+                                Navigator.pop(widget.context);
+                                Scaffold.of(widget.context).showSnackBar(
+                                    SnackBar(
+                                        content: Text(
+                                            "Loading article...",
+                                            textAlign: TextAlign.center),
+                                    )
+                                );
+                                _readArticle(widget.article);
+                              },
+                          ),
+                      )
                   )
-                ]
-            ),
+              )
+            ],
           ),
       initialChildSize: 0.3,
       maxChildSize: 0.5,
       expand: false,
     );
   }
+}
+
+class _FetchPageInfoResult {
+
+  final String uri;
+
+  const _FetchPageInfoResult({this.uri});
+
+  factory _FetchPageInfoResult.fromJson(Map<String, dynamic> json) =>
+      _FetchPageInfoResult(uri: json['canonicalurl']);
 }
